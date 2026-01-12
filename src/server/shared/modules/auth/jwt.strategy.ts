f1dd614c71f9c848fe38428a2527cbff68ea004b
Reilly Goulding <reilly.steere@gmail.com>
2026-01-11 21:11:35 -0700

    JWT TS Error

--- src/server/shared/modules/auth/jwt.strategy.ts
+++ src/server/shared/modules/auth/jwt.strategy.ts
@@ -15,6 +15,9 @@ export interface IJwtStrategy {
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
+    if (!process.env.JWT_AUTH_SECRET) {
+      throw new Error('JWT_AUTH_SECRET is not defined');
+    }
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
